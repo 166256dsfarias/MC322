32518734bd88cf098112375483a3fc1e6a51e35c
Daniel Santos de Farias <ra166256@students.ic.unicamp.br>
2025-09-03 00:13:49 -0300

    Cria classe abstrata Personagem

---
+++ tarefa1/src/Personagem.java
@@ -0,0 +1,28 @@
+public abstract class Personagem {
+    // Inspirado no universo de Harry Potter, todos os personagens serão MAGOS
+    protected String nome;
+    protected int pontosDeVida;
+    protected int forca;
+
+    //Construtor
+    public Personagem (String nome, int pontosDeVida, int forca){
+        this.nome = nome;
+        this.pontosDeVida = pontosDeVida;
+        this.forca = forca;
+    }
+
+    public void receberDano (int dano){
+        this.pontosDeVida -= dano;
+        this.pontosDeVida = this.pontosDeVida < 0 ? 0 : pontosDeVida; //evitar que alguem fique com vida negativa
+    }
+
+    public void exibirStatus (){
+        System.out.println("Nome: "+ nome);
+        System.out.println("Vida: "+ pontosDeVida);
+    }
+
+    // Contrato que as subclasses devem implementar
+    public abstract void atacar(Personagem alvo);
+
+
+}
